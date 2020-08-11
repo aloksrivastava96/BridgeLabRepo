93586852c1faf1dddf823f43f8fd01a26fd1c80b
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-11 16:53:35 +0530

    Last Name Validation Added

--- userregistration.sh
+++ userregistration.sh
@@ -2,10 +2,11 @@
 
 echo "Welcome to login page"
 read -p "First Name:" fname
-fnamepat="^[A-Z]{1}[a-zA-Z]{2,}$"
-if [[ $fname =~ $fnamepat ]]
+read -p "Last Name:" lname
+namepat="^[A-Z]{1}[a-zA-Z]{2,}$"
+if [[ $fname =~ $namepat && $lname =~ $namepat ]]
 then
-        echo "Welcome "$fname
+        echo "Welcome "$fname" "$lname
 else
         echo "Sorry, Incorrect User Detail(s)"
 fi
