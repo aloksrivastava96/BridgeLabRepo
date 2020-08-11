11c4c29f6838d62be951d798258dbcf6612a49c5
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-11 23:36:28 +0530

    Special Char added to password

--- userregistration.sh
+++ userregistration.sh
@@ -9,8 +9,8 @@ read -p "Password:" pass
 namepat="^[A-Z]{1}[a-zA-Z]{2,}$"
 emailpat="^[0-9a-zA-Z]+(.[0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
 mobilepat="^[0-9]{2}[ ][0-9]{10}$"
-passpat="^([a-zA-Z]*[A-Z]+[a-zA-Z]*){8,}"
-if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat && ${#pass} -ge 8 && "$pass" == *[[:upper:]]* && "$pass" == *[0-9]* ]]
+passpat=$(($(tr -d '[[:alnum:]]' <<< $pass | wc -m)-1))
+if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat && ${#pass} -ge 8 && $pass == *[[:upper:]]* && $pass == *[0-9]* && $passpat -eq 1 ]]
 then
         echo "Welcome "$fname" "$lname
 else
