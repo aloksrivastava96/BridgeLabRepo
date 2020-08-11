3c311fa090f7446e3f0730872217a8d30f0b7ec2
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-11 17:21:27 +0530

    EMAIL validation

--- userregistration.sh
+++ userregistration.sh
@@ -3,8 +3,10 @@
 echo "Welcome to login page"
 read -p "First Name:" fname
 read -p "Last Name:" lname
+read -p "Email:" email
 namepat="^[A-Z]{1}[a-zA-Z]{2,}$"
-if [[ $fname =~ $namepat && $lname =~ $namepat ]]
+emailpat="^[0-9a-zA-Z]+(.[0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$" 
+if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat ]]
 then
         echo "Welcome "$fname" "$lname
 else
