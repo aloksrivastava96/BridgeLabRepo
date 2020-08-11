00b9fee0da3c999f3273338816dc9ce2c39edb62
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-12 01:06:13 +0530

    Email Validation DONE

--- userregistration.sh
+++ userregistration.sh
@@ -7,7 +7,7 @@ read -p "Email:" email
 read -p "Mobile:" mobile
 read -p "Password:" pass
 namepat="^[A-Z]{1}[a-zA-Z]{2,}$"
-emailpat="^[0-9a-zA-Z]+(.[0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+emailpat="^[0-9a-zA-Z]+[_]*[+]*[-]*([.][0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{3})*$"
 mobilepat="^[0-9]{2}[ ][0-9]{10}$"
 passpat=$(($(tr -d '[[:alnum:]]' <<< $pass | wc -m)-1))
 if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat && ${#pass} -ge 8 && $pass == *[[:upper:]]* && $pass == *[0-9]* && $passpat -eq 1 ]]
