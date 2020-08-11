d27f0547a0d301be1968e3e36c66ae623015ab36
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-11 18:09:46 +0530

    Atleast 8 char in password

--- userregistration.sh
+++ userregistration.sh
@@ -5,10 +5,12 @@ read -p "First Name:" fname
 read -p "Last Name:" lname
 read -p "Email:" email
 read -p "Mobile:" mobile
+read -p "Password:" pass
 namepat="^[A-Z]{1}[a-zA-Z]{2,}$"
 emailpat="^[0-9a-zA-Z]+(.[0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
 mobilepat="^[0-9]{2}[ ][0-9]{10}$"
-if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat ]]
+passpat="^[a-zA-Z]{8,}"
+if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat && $pass =~ $passpat ]]
 then
         echo "Welcome "$fname" "$lname
 else
