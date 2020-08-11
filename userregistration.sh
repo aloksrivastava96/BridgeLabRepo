26bb246ee92ad8f56fd284436a5a9ae0c542a9a2
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-11 16:37:17 +0530

    User First Name Validation

--- userregistration.sh
+++ userregistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome to login page"
+read -p "First Name:" fname
+fnamepat="^[A-Z]{1}[a-zA-Z]{2,}$"
+if [[ $fname =~ $fnamepat ]]
+then
+        echo "Welcome "$fname
+else
+        echo "Sorry, Incorrect User Detail(s)"
+fi
