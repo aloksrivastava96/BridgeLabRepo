d4f36b3d3f94a3d952760aa4eec904f0ee508d4b
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-11 18:41:36 +0530

    Atleast one upper case

--- userregistration.sh
+++ userregistration.sh
@@ -9,8 +9,8 @@ read -p "Password:" pass
 namepat="^[A-Z]{1}[a-zA-Z]{2,}$"
 emailpat="^[0-9a-zA-Z]+(.[0-9a-zA-z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
 mobilepat="^[0-9]{2}[ ][0-9]{10}$"
-passpat="^[a-zA-Z]{8,}"
-if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat && $pass =~ $passpat ]]
+passpat="^([a-zA-Z]*[A-Z]+[a-zA-Z]*){8,}"
+if [[ $fname =~ $namepat && $lname =~ $namepat && $email =~ $emailpat && $mobile =~ $mobilepat && ${#pass} -ge 8 && "$pass" == *[[:upper:]]* ]]
 then
         echo "Welcome "$fname" "$lname
 else
